8dc9e1cff80ae2abb946d7c78ca2e48d9d03df37
ireisme <ireisme@gmail.com>
2019-04-12 13:42:36 -0400

    Fixed count

--- app/count.js
+++ app/count.js
@@ -3,6 +3,23 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function () {
   return {
     count : function (start, end) {
+      let shouldStop = false;
+      let current = start;
+
+      function increment() {
+        console.log(current);
+
+        if(current !== end && !shouldStop) {
+          current = current + 1;
+          setTimeout(increment, 100);
+        }
+      }
+
+      increment();
+
+      return {
+        cancel: () => shouldStop = true
+      };
     }
   };
 });
