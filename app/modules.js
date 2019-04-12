dccc08a43ef8f4e09a536c30af57ea7af42da3dc
ireisme <ireisme@gmail.com>
2019-04-12 13:09:27 -0400

    Fixed modules

--- app/modules.js
+++ app/modules.js
@@ -3,8 +3,16 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     createModule : function(str1, str2) {
+      return (function (){
+        let module = {
+          greeting: str1,
+          name: str2,
+        };
 
+        module.sayIt = () => `${module.greeting}, ${module.name}`;
+
+        return module;
+      }());
     }
   };
 });
-
